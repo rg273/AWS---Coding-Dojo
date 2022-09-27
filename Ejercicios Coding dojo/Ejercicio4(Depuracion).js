9d43ce8d76eb6986e3a2e7b2f7bcac0716885492
rodrigod <rodrigogutierrez273@gmail.com>
2022-09-27 00:45:41 -0300

    ejercicio 4 terminado

---
+++ Ejercicios Coding dojo/Ejercicio4(Depuracion).js
@@ -0,0 +1,14 @@
+
+/*
+Desafío de código: Depuración
+Usando lo que sabes sobre las funciones, variables, console.log y ahora return, arregla el siguiente código.
+*/
+
+function greeting(){
+
+    return "Hola, mundo";
+};
+
+let word = greeting();
+
+console.log(word)
