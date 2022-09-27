35187baeed128b3e5c6f16da346008b795d2e2d6
rodrigod <rodrigogutierrez273@gmail.com>
2022-09-27 02:14:44 -0300

    ejercicio 6 terminado

---
+++ Ejercicios Coding dojo/Ejercicio6/APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,38 @@
+
+//Intenta predecir que hara cada bloque de codgio
+
+// function myByrthYearFunc(){
+//     console.log("Naci en" + 1980)
+// }
+
+// Lo que va a hacer es, imprimir en consola Naci en 1980
+// Si la mandamos a llamar obviamente
+
+
+/*2
+Entonces, si la variable birthYearInput es 1980 (también conocida como var birthYearInput = 1980), y se llama a esta función, ¿qué indicará console.log?
+*/
+
+function myBirthYearFunc(birthYearInput){
+    console.log("Naci en" + birthYearInput);
+}
+
+//Imprimira en consola Naci en + el parametro que le pasemos a la funcion, en este caso 1980
+
+
+/*3
+Intentémoslo otra vez. Si var num1 = 10 y var num2 = 20, ¿qué indicaría console.log?
+*/
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 es: " + num1);
+    console.log("num2 es: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}    
+// El primer console.log = ¡Sumando números!
+// El segundo console.log 10
+// El tercer console.log seria 20
+// El console.log final seria igual a 30   
+
