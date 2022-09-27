f177aa09f9a26270d0b5e5768b28f71092cc696e
rodrigod <rodrigogutierrez273@gmail.com>
2022-09-26 21:48:42 -0300

    ejercicio 2 realizado

---
+++ Ejercicios Coding dojo/Ejercicio2.js
@@ -0,0 +1,24 @@
+/*
+Paso 1: revisa y edita tus variables segun sea necesario
+¿Son precisas tus variables? Actualízalas si es necesario.
+
+Paso 2: crea un condicional donde:
+Si la altura de la persona es mayor a 52, el console.log debe decir "¡Súbete, chico!". Si no, console.log debe mostrar "Lo siento, chico. Tal vez el próximo año".
+
+*/
+
+// Mis variables del ejercicio 1
+
+//tipo NUMBER
+let edad = 11;
+
+//tipo NUMBER
+let altura = 1.12
+
+if (edad > 52 ){
+    console.log("¡Súbete, chico!")
+}else{
+    console.log("Lo siento, chico. Tal vez el próximo año")
+}
+
+//En este caso la consola mostrara "Lo siento, chico. Tal vez el próximo año"
